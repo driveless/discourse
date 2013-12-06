841df7205870ef75416e2617628a313d24f7d867
Philip Kallberg <philip@cloud66.com>
2013-12-06 11:27:44 +0000

    update to unicorn
    c175009675d9f93a18242b3438ba79b357085f85
    
    update
    
    Conflicts:
    	config/cloud/cloud66/deploy_hooks.yml
    	config/cloud/cloud66/files/redis.yml

--- .cloud66/files/unicorn.rb
+++ .cloud66/files/unicorn.rb
@@ -1,4 +1,6 @@
 worker_processes 2
+ENV['UNICORN_ENABLE_OOBGC'] = "1"
+discourse_path = File.expand_path(File.expand_path(File.dirname(__FILE__)) + "/../")
 
 working_directory "#{ENV['RAILS_STACK_PATH']}"
 
@@ -18,6 +20,15 @@ GC.respond_to?(:copy_on_write_friendly=) and
 check_client_connection false
 
 before_fork do |server, worker|
+    I18n.t(:posts)
+    (ActiveRecord::Base.connection.tables - %w[schema_migrations]).each do |table|
+      table.classify.constantize.first rescue nil
+    end    
+    Rails.application.routes.recognize_path('abc') rescue nil
+    GC.start
+    initialized = true
+
+
     old_pid = '/tmp/web_server.pid.oldbin'
     if File.exists?(old_pid) && server.pid != old_pid
         begin
@@ -29,9 +40,13 @@ before_fork do |server, worker|
 
     defined?(ActiveRecord::Base) and
         ActiveRecord::Base.connection.disconnect!
+        $redis.client.disconnect
 end
 
 after_fork do |server, worker|
     defined?(ActiveRecord::Base) and
         ActiveRecord::Base.establish_connection
+        $redis.client.reconnect
+        Rails.cache.reconnect
+        MessageBus.after_fork
 end
