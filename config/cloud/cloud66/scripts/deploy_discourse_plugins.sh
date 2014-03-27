e352ae7685cf4c011b9b3696ccc458f5cd80e87d
williscool <wharris@upscalews.com>
2014-03-26 18:01:29 -0700

    Update deploy_discourse_plugins.sh
    no need to use the update plugin rake task.
    
    not to mention it doesn't work because the old plugins are not git repos :(

--- config/cloud/cloud66/scripts/deploy_discourse_plugins.sh
+++ config/cloud/cloud66/scripts/deploy_discourse_plugins.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd $RAILS_STACK_PATH
 bundle exec rake plugin:install repo=https://github.com/driveless/discourse-easy-signup
-bundle exec rake plugin:update_all
+# this is unnecessary since the capistrano task will always build a fresh app path from scratch
+# bundle exec rake plugin:update_all
